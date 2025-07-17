0a8cb64d76cf1436230170120fba3885c89d77ae
Luisgrande717 <luisgrande717@outlook.com>
2025-07-17 00:32:19 -0400

    updated Hero section with church image and updated some css to scale with other devices

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -5,14 +5,12 @@ const HeroSection = () => {
   return (
     <section className="hero">
       <div className="hero-overlay">
-        <h1>Welcome to St. Martin de Porres Parish</h1>
-        <p className="verse">
-          “Let all that you do be done in love.” – 1 Corinthians 16:14
-        </p>
+        <h1>Welcome to Our Lady of Fatima Parish</h1>
+        <p className="verse">"Many Cultures, One Faith"</p>
         <div className="hero-buttons">
-          <button>View Bulletin</button>
-          <button>Mass Schedule</button>
-          <button>Submit a Prayer</button>
+          <button>Mass Times</button>
+          <button>Events</button>
+          <button>Donate</button>
         </div>
       </div>
     </section>
